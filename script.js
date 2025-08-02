0141125f7c559948994766692aa51b2f0f0993de
Sevulik <srcseval@gmail.com>
2025-08-02 15:09:57 +0500

    js normativlar

---
+++ script.js
@@ -0,0 +1,44 @@
+// js 2.2
+
+// let arr = [
+//     {age: 40},
+//     {age: 20},
+//     {age: 18},
+//     {age: 30},
+//     {age: 21}
+// ]
+// arr.sort((a,b) => a.age - b.age)
+// console.log(arr);
+
+
+// js 2.3
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('FizzBuzz');
+//     }
+//     else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// js 2.4
+
+// let arr = [1, 5, 7, 9]
+// if (arr.includes(7)) {
+//     let res = arr.indexOf(7)
+//     console.log(res);
+// } else {
+//     console.log(-1);
+// }
+
+
+// js 2.5
+
+// let ask = prompt('Enter any word: ')
+// const palindrom = ask.split('').reverse().join('')
+// console.log(palindrom === ask);
